a6e221b855d82096b6261a19eee329c947c206fd
Хуршман Доминика <hristinahursman@iMac-HurshmanDKH.local>
2023-05-22 15:32:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myview/MyView.java
@@ -0,0 +1,39 @@
+package com.example.myview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+
+    public MyView(Context context) {
+        super(context);
+        for (int i = 0; i < N; i++) {
+            x[i] = (float) (Math.random() * 500);
+            y[i] = (float) (Math.random() * 500);
+            vx[i] = (float) (Math.random() * 6 - 3);
+            vy[i] = (float) (Math.random() * 6 - 3);
+        }
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        Paint paint = new Paint();
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        invalidate();
+    }
+}
